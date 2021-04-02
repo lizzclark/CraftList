6f791f968620d9658b4e594db3c1e974c57c0190
Lizz Clark <lizz.clark@moonpig.com>
2021-04-02 10:14:02 +0100

    Initial Commit

---
+++ CraftList/CraftListApp.swift
@@ -0,0 +1,20 @@
+//
+//  CraftListApp.swift
+//  CraftList
+//
+//  Created by Lizz Clark on 02/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct CraftListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
